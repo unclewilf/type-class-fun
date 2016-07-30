62ad279e32bcd64bdc08ad7e89e9207c5c756abe
will <w.f.funnell@gmail.com>
2016-07-31 00:28:48 +0100

    Added some test cases

--- src/main/scala/numerals/NumeralConverter.scala
+++ src/main/scala/numerals/NumeralConverter.scala
@@ -1,5 +1,36 @@
 package numerals
 
-class NumeralConverter {
+object NumeralConverter {
 
+    val numberToNumeral = Map(
+        10 -> "X",
+        5 -> "V",
+        1 -> "I"
+    )
+
+    def convert(i: Int): String = {
+
+        val exact = getExact(i)
+
+        handle3InARow(exact)
+    }
+
+    def getExact(i: Int): String = {
+
+        numberToNumeral.collectFirst {
+            case (number, numeral) if i == number => numeral
+            case (number, numeral) if i >= number => numeral + getExact(i - number)
+        }.getOrElse(throw new IllegalArgumentException)
+    }
+
+    def handle3InARow(numerals: String): String = {
+
+        println(s"removing 3 in a row from numerals=$numerals")
+
+        if (numerals.matches("V([I])\\1{3}")) {
+            "IX"
+        } else {
+            numerals.replaceFirst("([I])\\1{3}", "IV")
+        }
+    }
 }
--- src/test/scala/numerals/NumeralConverterTest.scala
+++ src/test/scala/numerals/NumeralConverterTest.scala
@@ -1,7 +1,31 @@
 package numerals
 
-import org.scalatest.FunSuite
+import numerals.NumeralConverter.convert
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{Matchers, FunSuite}
 
-class NumeralConverterTest extends FunSuite {
+class NumeralConverterTest extends FunSuite with Matchers with TableDrivenPropertyChecks {
 
+    val numeralConversions =
+        Table(
+            ("Arabic", "Numeral"),
+            ( 1,       "I"),
+            ( 3,       "III"),
+            ( 5,       "V"),
+            ( 8,       "VIII"),
+            ( 9,       "IX"),
+            ( 10,      "X")
+        )
+
+//        9	    IX
+//        1066	MLXVI
+//        1989	MCMLXXXIX
+
+    forAll (numeralConversions) { (arabic: Int, numeral: String) =>
+        convert(arabic) shouldBe numeral
+    }
+
+    test("I can only be repeated at most 3 times in a row") {
+        convert(4) shouldBe "IV"
+    }
 }
